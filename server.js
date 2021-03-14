df794982e7df4113bded12e8d265c8678e071dad
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-14 15:51:07 -0700

    add htmlRoutes

--- server.js
+++ server.js
@@ -2,18 +2,26 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require("morgan");
+
+//const db = require("./models");
 
 const app = express();
 
-//const routes;
+app.use(logger("dev"));
 
 app.use(express.static("public"));
 
 // Parsing
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+//require("./routes/api")(app);
+require("./routes/htmlRoutes")(app);
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance");
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
