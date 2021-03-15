78a9e9d884a742db5f88b4b9d597282b71452495
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-15 08:53:12 -0700

    add type to resistance model

--- models/resistance.js
+++ models/resistance.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const resistanceSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+    },
     name: {
         type: String,
         required: true,
