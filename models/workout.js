79b6eaaad1cced3f61a9b27e9780f3f031c80327
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-15 10:29:04 -0700

    remove cardio model, update resistance model to work for all workout types

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now(),
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: true,
+            enum: ["resistance", "cardio"],
+            },
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            maxLength: [50, "Exercise name must be shorter than fifty characters."],
+        },
+        weight: {
+            type: Number,
+            required: false,
+            min: [1, "weight must be 1 or greater."],
+        },
+        sets: {
+            type: Number,
+            required: false,
+            min: [1, "You must have at least one set."],
+        },
+        reps: {
+            type: Number,
+            required: false,
+            min: [1, "You must have at least one rep."],
+        },
+        duration: {
+            type: Number,
+            max: 60,
+            required: true,
+        },
+        distance: {
+            type: Number,
+            max: 10000,
+            required: false,
+        },
+    }],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
