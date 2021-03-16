64d0007e72833fb9cecd1d4e9512d9f883e4e6d9
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-16 13:59:45 -0700

    update get range route to display workout stats.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,20 +8,21 @@ module.exports = (app) => {
 
     app.put("/api/workouts/:id", async (req, res) => {
         console.log("update query body is", req.body);
-        // const result = await db.Workout.updateOne({ id: req.params.id }, {$push: {exercises: [req.body]}})
-        // console.log(result);
-        // res.json(result);
+        const result = await db.Workout.updateOne({ id: req.params.id }, {$push: {exercises: [req.body]}})
+        console.log(result);
+        res.json(result);
     });
 
     app.post("/api/workouts", async (req, res) => {
         console.log("post query body is", req.body);
-        const w = await db.Workout.createCollection({...req.body});
+        const w = await db.Workout.create({...req.body});
         console.log(w)
         res.json(w);
     });
 
     app.get("/api/workouts/range", async (req, res) => {
-        res.send("hello");
+        let stats = await db.Workout.find({});
+        res.json(stats);
         console.log('range accessed');
     })
 }
